3cc93c159e5a351a387bcd78dad77a5dc298c7d6
Mark Oates <mark.oates@shopify.com>
2018-07-02 13:16:56 -0400

    Add CppFunctionArgument

---
+++ include/Blast/CppFunctionArgument.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+
+#include <string>
+
+
+namespace Blast
+{
+   class CppFunctionArgument
+   {
+   private:
+      std::string type;
+      std::string name;
+      std::string default_value;
+
+   public:
+      CppFunctionArgument(std::string type="std::string", std::string name="unnamed_function", std::string default_value="\"\"");
+      ~CppFunctionArgument();
+
+
+      std::string get_type();
+      std::string get_name();
+      std::string get_default_value();
+   };
+}
+
+
+
