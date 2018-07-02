4a8b0dda66a46845a31dc5cd590a0382de31040a
Mark Oates <mark.oates@shopify.com>
2018-07-01 21:10:09 -0400

    Add CppClass (generated)

---
+++ include/Blast/CppClass.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+
+#include <Blast/ClassAttributeProperties.hpp>
+#include <Blast/ParentClassProperties.hpp>
+#include <Blast/SymbolDependencies.hpp>
+#include <string>
+#include <vector>
+
+
+namespace Blast
+{
+   class CppClass
+   {
+   private:
+      std::string class_name;
+      std::vector<std::string> namespaces;
+      std::vector<Blast::ParentClassProperties> parent_class_properties;
+      std::vector<Blast::ClassAttributeProperties> attribute_properties;
+      std::vector<Blast::SymbolDependencies> symbol_dependencies;
+
+   public:
+      CppClass(std::string class_name="UnnamedClass", std::vector<std::string> namespaces={}, std::vector<Blast::ParentClassProperties> parent_class_properties={}, std::vector<Blast::ClassAttributeProperties> attribute_properties={}, std::vector<Blast::SymbolDependencies> symbol_dependencies={});
+      ~CppClass();
+
+
+      std::string get_class_name();
+      std::vector<std::string> get_namespaces();
+      std::vector<Blast::ParentClassProperties> get_parent_class_properties();
+      std::vector<Blast::ClassAttributeProperties> get_attribute_properties();
+      std::vector<Blast::SymbolDependencies> get_symbol_dependencies();
+   };
+}
+
+
---
+++ src/Blast/CppClass.cpp
@@ -0,0 +1,57 @@
+
+
+#include <Blast/CppClass.hpp>
+
+
+namespace Blast
+{
+
+
+CppClass::CppClass(std::string class_name, std::vector<std::string> namespaces, std::vector<Blast::ParentClassProperties> parent_class_properties, std::vector<Blast::ClassAttributeProperties> attribute_properties, std::vector<Blast::SymbolDependencies> symbol_dependencies)
+   : class_name(class_name)
+   , namespaces(namespaces)
+   , parent_class_properties(parent_class_properties)
+   , attribute_properties(attribute_properties)
+   , symbol_dependencies(symbol_dependencies)
+{
+}
+
+
+CppClass::~CppClass()
+{
+}
+
+
+std::string CppClass::get_class_name()
+{
+   return class_name;
+}
+
+
+std::vector<std::string> CppClass::get_namespaces()
+{
+   return namespaces;
+}
+
+
+std::vector<Blast::ParentClassProperties> CppClass::get_parent_class_properties()
+{
+   return parent_class_properties;
+}
+
+
+std::vector<Blast::ClassAttributeProperties> CppClass::get_attribute_properties()
+{
+   return attribute_properties;
+}
+
+
+std::vector<Blast::SymbolDependencies> CppClass::get_symbol_dependencies()
+{
+   return symbol_dependencies;
+}
+
+
+}
+
+
