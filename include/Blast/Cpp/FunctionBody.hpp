107d1b46608ae9d217241d6ab7e025e54a62d76e
Mark Oates <mark.oates@shopify.com>
2018-12-17 01:22:34 -0500

    Add "Blast::Cpp::FunctionBody"

---
+++ include/Blast/Cpp/FunctionBody.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+
+#include <string>
+#include <vector>
+
+
+namespace Blast
+{
+   namespace Cpp
+   {
+      class FunctionBody
+      {
+      private:
+         std::string content;
+         std::vector<std::string> dependency_symbols;
+
+      public:
+         FunctionBody(std::string content="return;", std::vector<std::string> dependency_symbols={});
+         ~FunctionBody();
+
+         void set_content(std::string content);
+         void set_dependency_symbols(std::vector<std::string> dependency_symbols);
+
+         std::string get_content();
+         std::vector<std::string> get_dependency_symbols();
+      };
+   }
+}
+
+
+
