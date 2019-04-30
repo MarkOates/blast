aeff2c178fae1e3c057e4eeb425107ff4b22fe25
Mark Oates <markoates0@gmail.com>
2019-04-29 21:38:47 -0400

    Rename variable

--- tests/Blast/DirectoryCreatorTest.cpp
+++ tests/Blast/DirectoryCreatorTest.cpp
@@ -13,16 +13,17 @@ TEST(DirectoryCreatorTest, can_be_created_without_arguments)
 
 TEST(DirectoryCreatorTest, create__returns_true_when_a_directory_is_present)
 {
-   std::vector<std::string> directory_that_should_exist = { "quintessence" };
-   Blast::DirectoryCreator directory_creator(directory_that_should_exist);
+   std::vector<std::string> directories_that_should_exist = { "quintessence" };
+
+   Blast::DirectoryCreator directory_creator(directories_that_should_exist);
    ASSERT_TRUE(directory_creator.create());
 }
 
 
 TEST(DirectoryCreatorTest, create__returns_false_when_attempting_to_creating_multiple_nested_directories)
 {
-   std::vector<std::string> directory_that_should_exist = { "directory_that_does_not_exist/and_has_a_nested_directory/" };
-   Blast::DirectoryCreator directory_creator(directory_that_should_exist);
+   std::vector<std::string> directories_that_should_exist = { "directory_that_does_not_exist/and_has_a_nested_directory/" };
+   Blast::DirectoryCreator directory_creator(directories_that_should_exist);
    ASSERT_FALSE(directory_creator.create());
 }
 
