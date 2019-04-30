ed9c5163facbf7feec297250163b88956b33a69b
Mark Oates <markoates0@gmail.com>
2019-04-29 21:40:47 -0400

    Add setup asertions for test

--- tests/Blast/DirectoryCreatorTest.cpp
+++ tests/Blast/DirectoryCreatorTest.cpp
@@ -5,6 +5,14 @@
 #include <Blast/DirectoryCreator.hpp>
 
 
+#include <fstream>
+bool file_exists(const std::string& filename)
+{
+  std::ifstream ifile(filename.c_str());
+  return (bool)ifile;
+}
+
+
 TEST(DirectoryCreatorTest, can_be_created_without_arguments)
 {
    Blast::DirectoryCreator directory_creator;
@@ -15,6 +23,9 @@ TEST(DirectoryCreatorTest, create__returns_true_when_a_directory_is_present)
 {
    std::vector<std::string> directories_that_should_exist = { "quintessence" };
 
+   for (auto &directory_that_should_exist : directories_that_should_exist)
+      ASSERT_TRUE(file_exists(directory_that_should_exist));
+
    Blast::DirectoryCreator directory_creator(directories_that_should_exist);
    ASSERT_TRUE(directory_creator.create());
 }
@@ -23,6 +34,10 @@ TEST(DirectoryCreatorTest, create__returns_true_when_a_directory_is_present)
 TEST(DirectoryCreatorTest, create__returns_false_when_attempting_to_creating_multiple_nested_directories)
 {
    std::vector<std::string> directories_that_should_exist = { "directory_that_does_not_exist/and_has_a_nested_directory/" };
+
+   for (auto &directory_that_should_exist : directories_that_should_exist)
+      ASSERT_FALSE(file_exists(directory_that_should_exist));
+
    Blast::DirectoryCreator directory_creator(directories_that_should_exist);
    ASSERT_FALSE(directory_creator.create());
 }
