db0e2a7d05ba21d477b30aaf0891d78afe8e134c
Mark Oates <markoates0@gmail.com>
2019-12-01 13:20:38 -0500

    Add (empty) ProjectComponentBasenameExtractor test file

---
+++ tests/Blast/ProjectComponentBasenameExtractorTest.cpp
@@ -0,0 +1,13 @@
+
+
+#include <gtest/gtest.h>
+
+#include <Blast/ProjectComponentBasenameExtractor.hpp>
+
+
+TEST(Blast_ProjectComponentBasenameExtractor, can_be_created_without_arguments)
+{
+   Blast::ProjectComponentBasenameExtractor extractor;
+}
+
+
