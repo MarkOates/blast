b22bc6614bc7b280c0576de1559ee4ce04816cd1
Mark Oates <markoates0@gmail.com>
2019-12-01 13:33:07 -0500

    Fix overlooked renaming "CamelCaseToUnderscoreConverter" in test file

--- tests/Blast/CamelCaseToUnderscoreConverterTest.cpp
+++ tests/Blast/CamelCaseToUnderscoreConverterTest.cpp
@@ -1,12 +1,12 @@
 
 #include <gtest/gtest.h>
 
-#include <Blast/CamelCaseUnderscoreConverter.hpp>
+#include <Blast/CamelCaseToUnderscoreConverter.hpp>
 
 
-TEST(Blast_CamelCaseUnderscoreConverterTest, convert_to_underscores__returns_the_expected_string)
+TEST(Blast_CamelCaseToUnderscoreConverterTest, convert_to_underscores__returns_the_expected_string)
 {
-   Blast::CamelCaseUnderscoreConverter converter("ThisIsTheComponentName");
+   Blast::CamelCaseToUnderscoreConverter converter("ThisIsTheComponentName");
    std::string expected_string = "this_is_the_component_name";
    EXPECT_EQ(expected_string, converter.convert_to_underscores());
 }
