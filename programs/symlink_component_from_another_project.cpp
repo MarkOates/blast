d7cecdf3160632ca6726cc96244ffde9c8302704
Mark Oates <markoates0@gmail.com>
2019-12-01 15:40:26 -0500

    Add "symlink_component_from_another_project" program

---
+++ programs/symlink_component_from_another_project.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <Blast/TemplatedFile.hpp>
+
+
+std::vector<std::string> args;
+
+
+int main(int argc, char **argv)
+{
+   for (int i=0; i<argc; i++) args.push_back(argv[i]);
+   if (args.size() != 3) throw std::runtime_error("You must pass a target project name, follwed by a component name");
+
+   const std::string COMMAND_TEMPLATE = "TARGET_PROJECT=[[TARGET_PROJECT]]; COMPONENT=[[COMPONENT]]; " \
+                                        "ln -s /Users/markoates/Repos/${TARGET_PROJECT}/quintessence/${COMPONENT}.q.yml " \
+                                        "[[TARGET_PROJECT_FOLDER_NAME]]/quintessence/${COMPONENT}.q.yml; " \
+                                        "unset COMPONENT; unset TARGET_PROJECT";
+
+   std::string source_project_raw_folder_name = args[0]; //"blast";
+   std::string component_name = args[1]; //"Blast/ShellCommandExecutorWithCallback";
+   std::string target_project_raw_folder_name = ".";
+
+   Blast::TemplatedFile templated_file(COMMAND_TEMPLATE, {
+      { "[[TARGET_PROJECT]]",             source_project_raw_folder_name },
+      { "[[COMPONENT]]",                  component_name },
+      { "[[TARGET_PROJECT_FOLDER_NAME]]", target_project_raw_folder_name },
+   });
+
+   std::string result_command = templated_file.generate_content();
+
+   std::cout << result_command << std::endl;
+}
+
+
