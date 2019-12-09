491ea6a821475a36ee08489ee603310b96315d6a
Mark Oates <mark.oates@shopify.com>
2019-12-09 17:17:56 -0500

    Modify file existence checker; Use lower-level implementation

--- src/Blast/FileExistenceChecker.cpp
+++ src/Blast/FileExistenceChecker.cpp
@@ -1,7 +1,10 @@
 
 
 #include <Blast/FileExistenceChecker.hpp>
-
+#include <experimental/filesystem>
+#include <sys/stat.h>
+#include <sys/stat.h>
+#include <sys/stat.h>
 
 
 namespace Blast
@@ -27,7 +30,8 @@ std::string FileExistenceChecker::get_filename()
 
 bool FileExistenceChecker::exists()
 {
-return true;
+struct stat info;
+return stat(filename.c_str(), &info) == 0 && (S_ISREG(info.st_mode) || S_ISLNK(info.st_mode));
 
 }
 } // namespace Blast
