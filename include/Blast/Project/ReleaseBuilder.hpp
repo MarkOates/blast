944db353539d4e2e6a736a5f94ef23be875e29b8
Mark Oates <markoates0@gmail.com>
2020-01-04 10:24:16 -0500

    Add scaffold for Project/ReleaseBuilder

--- include/Blast/Project/ReleaseBuilder.hpp
+++ include/Blast/Project/ReleaseBuilder.hpp
@@ -2,7 +2,6 @@
 
 
 #include <string>
-#include <vector>
 
 
 namespace Blast
@@ -12,23 +11,13 @@ namespace Blast
       class ReleaseBuilder
       {
       private:
-         std::string project_repo_name;
-         std::string project_repo_base_path;
 
       public:
-         ReleaseBuilder(std::string project_repo_name="", std::string project_repo_base_path="/Users/markoates/Repos/");
+         ReleaseBuilder();
          ~ReleaseBuilder();
 
 
-         std::string get_project_repo_base_path();
-      std::vector<std::string> get_include_file_listings();
-      std::vector<std::string> get_source_file_listings();
-      std::vector<std::string> build_repo_base_path_directory_components();
-      std::vector<std::string> build_include_folder_directory_components();
-      std::vector<std::string> build_src_folder_directory_components();
-      std::vector<std::string> build_data_folder_directory_components();
-      void duplicate_source_files();
-      void create_folders();
+      std::string run();
       };
    }
 }
--- src/Blast/Project/ReleaseBuilder.cpp
+++ src/Blast/Project/ReleaseBuilder.cpp
@@ -1,15 +1,7 @@
 
 
 #include <Blast/Project/ReleaseBuilder.hpp>
-#include <sstream>
-#include <iostream>
-#include <sstream>
-#include <iostream>
-#include <Blast/StringSplitter.hpp>
-#include <Blast/DirectoryCreator.hpp>
-#include <Blast/ShellCommandExecutorWithCallback.hpp>
-#include <iostream>
-#include <iostream>
+
 
 
 namespace Blast
@@ -18,9 +10,7 @@ namespace Project
 {
 
 
-ReleaseBuilder::ReleaseBuilder(std::string project_repo_name, std::string project_repo_base_path)
-   : project_repo_name(project_repo_name)
-   , project_repo_base_path(project_repo_base_path)
+ReleaseBuilder::ReleaseBuilder()
 {
 }
 
@@ -30,122 +20,9 @@ ReleaseBuilder::~ReleaseBuilder()
 }
 
 
-std::string ReleaseBuilder::get_project_repo_base_path()
+std::string ReleaseBuilder::run()
 {
-   return project_repo_base_path;
-}
-
-
-std::vector<std::string> ReleaseBuilder::get_include_file_listings()
-{
-std::stringstream command;
-
-command << "(cd " << project_repo_base_path << project_repo_name << " && " << "find include/* -type f)";
-
-std::cout << command.str() << std::endl;
-
-std::string response = Blast::ShellCommandExecutorWithCallback(
-   command.str(),
-   ShellCommandExecutorWithCallback::simple_silent_callback
-).execute();
-
-std::vector<std::string> response_lines = StringSplitter(response, '\n').split();
-
-return response_lines;
-
-}
-
-std::vector<std::string> ReleaseBuilder::get_source_file_listings()
-{
-std::stringstream command;
-
-command << "(cd " << project_repo_base_path << project_repo_name << " && " << "find src/* -type f)";
-
-std::cout << command.str() << std::endl;
-
-std::string response = Blast::ShellCommandExecutorWithCallback(
-   command.str(),
-   ShellCommandExecutorWithCallback::simple_silent_callback
-).execute();
-
-std::vector<std::string> response_lines = StringSplitter(response, '\n').split();
-
-return response_lines;
-
-}
-
-std::vector<std::string> ReleaseBuilder::build_repo_base_path_directory_components()
-{
-return StringSplitter(project_repo_base_path, '/').split();
-}
-
-std::vector<std::string> ReleaseBuilder::build_include_folder_directory_components()
-{
-std::vector<std::string> result = StringSplitter(project_repo_base_path, '/').split();
-result.push_back("releases");
-result.push_back("include");
-return result;
-
-}
-
-std::vector<std::string> ReleaseBuilder::build_src_folder_directory_components()
-{
-std::vector<std::string> result = StringSplitter(project_repo_base_path, '/').split();
-result.push_back("releases");
-result.push_back("src");
-return result;
-
-}
-
-std::vector<std::string> ReleaseBuilder::build_data_folder_directory_components()
-{
-std::vector<std::string> result = StringSplitter(project_repo_base_path, '/').split();
-result.push_back("releases");
-result.push_back("data");
-return result;
-
-}
-
-void ReleaseBuilder::duplicate_source_files()
-{
-std::vector<std::string> source_file_listings = get_source_file_listings();
-
-for (auto &source_file_listing : source_file_listings)
-{
-   std::vector<std::string> result = StringSplitter(project_repo_base_path, '/').split();
-   result.pop_back();
-   Blast::DirectoryCreator(result, true).create();
-}
-return;
-
-}
-
-void ReleaseBuilder::create_folders()
-{
-// make folder if not exists
-// releases/
-Blast::DirectoryCreator(build_include_folder_directory_components(), true).create();
-Blast::DirectoryCreator(build_src_folder_directory_components(), true).create();
-Blast::DirectoryCreator(build_data_folder_directory_components(), true).create();
-
-//if (result != 0) { throw std::runtime_error("could not create directories"); }
-
-// make folder if not exists
-// releases/source/
-
-// copy source files
-// find src/**/*
-
-// copy   releases/source/data/* files
-// copy   releases/source/include/* files
-// copy   releases/source/src/* files
-// create releases/source/Makefile
-
-//- al_load_bitmap
-//- al_load_font
-//- bitmaps
-//- fonts
-
+return "Hello World!";
 }
 } // namespace Project
 } // namespace Blast
