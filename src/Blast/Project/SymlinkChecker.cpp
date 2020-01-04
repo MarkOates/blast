cbb84065cd85eb84eebf862b83d75a3ce2120d5e
Mark Oates <markoates0@gmail.com>
2020-01-04 12:11:25 -0500

    Fix buffer overrun error when trying to read_symilnk_target

--- src/Blast/Project/SymlinkChecker.cpp
+++ src/Blast/Project/SymlinkChecker.cpp
@@ -38,12 +38,13 @@ return (S_ISLNK(p_statbuf.st_mode) == 1);
 
 std::string SymlinkChecker::read_symlink_target()
 {
-char linkname[256];
-// from https://linux.die.net/man/2/readlink
-//ssize_t readlink(const char *path, char *buf, size_t bufsiz);
-ssize_t r;
-r = readlink(filename.c_str(), linkname, 256);
-return linkname;
+char buff[512];
+ssize_t len = ::readlink(filename.c_str(), buff, sizeof(buff)-1);
+if (len != -1) {
+   buff[len] = '\0';
+   return std::string(buff);
+}
+throw std::runtime_error("an error occurred when trying to read_symlink_target in Blast/Project/SymlinkChecker");
 
 }
 } // namespace Project
