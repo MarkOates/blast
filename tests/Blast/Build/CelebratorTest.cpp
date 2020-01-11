c0adede363b2180f3e66ba6e9c550b01c1084eec
Mark Oates <markoates0@gmail.com>
2020-01-10 23:24:32 -0500

    Add test ensuring Build/Celebrator outputs varying lengths

--- tests/Blast/Build/CelebratorTest.cpp
+++ tests/Blast/Build/CelebratorTest.cpp
@@ -8,10 +8,20 @@ TEST(Blast_Build_CelebratorTest, can_be_created_without_blowing_up)
    Blast::Build::Celebrator celebrator;
 }
 
-TEST(Blast_Build_CelebratorTest, run__outputs_a_string_of_the_expected_length)
+TEST(Blast_Build_CelebratorTest, run__outputs_a_string_of_the_expected_default_length)
 {
    Blast::Build::Celebrator celebrator;
    EXPECT_EQ(80, celebrator.generate_full_width_output_banner("Hello World!").size());
+}
+
+TEST(Blast_Build_CelebratorTest, run__outputs_strings_of_varying_lengths)
+{
+   std::vector<int> lengths_to_attempt = { 30, 60, 83, 521 };
+   for (auto &length_to_attempt : lengths_to_attempt)
+   {
+      Blast::Build::Celebrator celebrator;
+      EXPECT_EQ(length_to_attempt, celebrator.generate_full_width_output_banner("Hello World!", length_to_attempt).size());
+   }
 }
 
 TEST(Blast_Build_CelebratorTest, run__returns_the_expected_response)
