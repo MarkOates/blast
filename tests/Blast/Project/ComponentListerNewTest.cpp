feb7fdd64a29636c024e184e01867fadb9031f8e
Mark Oates <mark.oates@shopify.com>
2020-01-21 11:26:53 -0500

    Rename "*_new" variables in Project/ComponentListerNew test

--- tests/Blast/Project/ComponentListerNewTest.cpp
+++ tests/Blast/Project/ComponentListerNewTest.cpp
@@ -5,12 +5,12 @@
 
 TEST(Blast_Project_ComponentListerNewTest, can_be_created_without_blowing_up)
 {
-   Blast::Project::ComponentListerNew component_lister_new;
+   Blast::Project::ComponentListerNew component_lister;
 }
 
 TEST(Blast_Project_ComponentListerNewTest, run__returns_the_expected_response)
 {
-   Blast::Project::ComponentListerNew component_lister_new;
+   Blast::Project::ComponentListerNew component_lister;
    std::string expected_string = "Hello World!";
-   EXPECT_EQ(expected_string, component_lister_new.run());
+   EXPECT_EQ(expected_string, component_lister.run());
 }
