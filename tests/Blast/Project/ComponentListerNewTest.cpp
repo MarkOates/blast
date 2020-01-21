6a27426e63bade67b7c10a0584eeb73ba093552c
Mark Oates <mark.oates@shopify.com>
2020-01-21 11:38:02 -0500

    Rename test with description of testing default behavior

--- tests/Blast/Project/ComponentListerNewTest.cpp
+++ tests/Blast/Project/ComponentListerNewTest.cpp
@@ -8,7 +8,7 @@ TEST(Blast_Project_ComponentListerNewTest, can_be_created_without_blowing_up)
    Blast::Project::ComponentListerNew component_lister;
 }
 
-TEST(Blast_Project_ComponentListerNewTest, components__returns_the_expected_values)
+TEST(Blast_Project_ComponentListerNewTest, components__returns_the_expected_values_when_no_arguments_are_given)
 {
    Blast::Project::ComponentListerNew component_lister;
    std::vector<std::string> expected = {};
