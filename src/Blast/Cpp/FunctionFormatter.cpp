dc71106bd16c01e4b47f3510e8140d7acbc72ee4
Mark Oates <markoates0@gmail.com>
2020-01-25 16:37:33 -0500

    Use "\n" as replacement for std::endl in Cpp/FunctionFormatter

--- src/Blast/Cpp/FunctionFormatter.cpp
+++ src/Blast/Cpp/FunctionFormatter.cpp
@@ -7,6 +7,9 @@
 #include <sstream>
 
 
+static const std::string ENDL = "\n";
+
+
 namespace Blast
 {
 
@@ -47,7 +50,7 @@ std::string FunctionFormatter::get_function_declaration()
    if (function.get_is_pure_virtual()) result << " = 0";
    if (function.get_is_const()) result << " const";
    if (function.get_is_override()) result << " override";
-   result << ';' << std::endl;
+   result << ';' << ENDL;
 
    return result.str();
 }
@@ -72,10 +75,10 @@ std::string FunctionFormatter::get_function_definition()
    if (!class_name.empty()) result << class_name << "::";
    result << function.get_name() << "(" << Blast::StringJoiner(function_arg_elements, ", ").join() << ")";
    if (function.get_is_const()) result << " const";
-   result << std::endl;
-   result << '{' << std::endl;
-   result << function.get_body() << std::endl;
-   result << '}' << std::endl;
+   result << ENDL;
+   result << '{' << ENDL;
+   result << function.get_body() << ENDL;
+   result << '}' << ENDL;
 
    return result.str();
 }
