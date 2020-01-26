41235f83a841316d5b3d08023fc37abbaf03cbed
Mark Oates <markoates0@gmail.com>
2020-01-26 17:38:59 -0500

    Disable functionality from Blast/DirectoryCreator (due to not compiling on Mac)

--- src/Blast/DirectoryCreator.cpp
+++ src/Blast/DirectoryCreator.cpp
@@ -25,20 +25,20 @@ DirectoryCreator::~DirectoryCreator()
 
 bool DirectoryCreator::create()
 {
-std::stringstream result_directories;
-const std::string SEPARATOR = "/";
-if (path_is_absolute) result_directories << "/";
-for (auto &directory_component : directory_components)
-{
-  result_directories << directory_component << SEPARATOR;
-  std::string directory_to_create = result_directories.str();
-
-  if (Blast::DirectoryExistenceChecker(directory_to_create).exists()) continue;
-  int mkdir_result = mkdir(directory_to_create.c_str());
-  //int mkdir_result = mkdir(directory_to_create.c_str(), 0777);
-  if (mkdir_result == -1) return false;
-}
-return true;
+//std::stringstream result_directories;
+//const std::string SEPARATOR = "/";
+//if (path_is_absolute) result_directories << "/";
+//for (auto &directory_component : directory_components)
+//{
+  //result_directories << directory_component << SEPARATOR;
+  //std::string directory_to_create = result_directories.str();
+  //if (Blast::DirectoryExistenceChecker(directory_to_create).exists()) continue;
+  //int mkdir_result = mkdir(directory_to_create.c_str());
+  ////int mkdir_result = mkdir(directory_to_create.c_str(), 0777);
+  //if (mkdir_result == -1) return false;
+//}
+//return true;
+return false;
 
 }
 } // namespace Blast
