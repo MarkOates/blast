db10eb6d1437e6e88b8d7a56c46eaa3b62661dbe
Mark Oates <mark.oates@shopify.com>
2020-03-02 15:21:59 -0500

    Pile up shopify dev mode changes to "developer" program

--- programs/developer.cpp
+++ programs/developer.cpp
@@ -6,6 +6,8 @@
 
 #include <Blast/ShellCommandExecutorWithCallback.hpp>
 
+
+
 void execute_command(std::string command)
 {
    Blast::ShellCommandExecutorWithCallback shell_command_executor(
@@ -20,22 +22,36 @@ int main(int argc, char **argv)
 {
    std::string user_input;
 
+   std::string folder = "/Users/markoates/dev_repos/partners";
+
+   std::cout << "  Mode: Shopify Dev Mode" << std::endl;
+   std::cout << "Folder: " << folder << std::endl;
+   std::cout << "====================" << std::endl;
+   std::cout << "Enter component name >> ";
+   getline(std::cin, user_input);
+   std::cout << std::endl;
+
    while(true)
    {
-      std::cout << ">> ";
-      getline(std::cin, user_input);
-      std::cout << std::endl;
+      std::string ruby_component_test_rerun_command = "COMPONENT_NAME=" + user_input + " && rerun -c \"bin/rails db:environment:set RAILS_ENV=test; bundle exec rspec spec/${COMPONENT_NAME}_spec.rb\"; unset COMPONENT_NAME";
+
+      std::cout << "Running command: " << ruby_component_test_rerun_command << std::endl;
 
       if (user_input == "exit") break;
 
       std::vector<std::string> command_tokens;
       command_tokens = {
          "(",
-            "cd /Users/markoates/Repos/blast",
+            //"cd /Users/markoates/Repos/blast",
+            "cd ",
+              folder,
             "&&",
-            user_input,
+            "(",
+               user_input,
+            ")",
          ")",
       };
+
 
       std::stringstream command;
       for (auto &command_token : command_tokens)
