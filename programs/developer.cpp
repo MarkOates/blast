ff7bcfb1ce915ccfbd1388f9027dbe654bdd4aa5
Mark Oates <mark.oates@shopify.com>
2020-03-03 13:11:01 -0500

    Allow passing in component name during initial develop startup

--- programs/developer.cpp
+++ programs/developer.cpp
@@ -28,7 +28,7 @@ std::string __replace(std::string str, std::string from, std::string to)
 
 int main(int argc, char **argv)
 {
-   std::string RUBY_DEV_COMMAND = "COMPONENT_NAME=models/review; rerun -c \"bin/rails db:environment:set RAILS_ENV=test; bundle exec rspec spec/${COMPONENT_NAME}_spec.rb\"; unset COMPONENT_NAME";
+   std::string RUBY_DEV_COMMAND = "COMPONENT_NAME=[[COMPONENT_NAME]]; rerun -c \"bin/rails db:environment:set RAILS_ENV=test; bundle exec rspec spec/${COMPONENT_NAME}_spec.rb\"; unset COMPONENT_NAME";
 
    std::string user_input;
    while(true)
@@ -39,12 +39,17 @@ int main(int argc, char **argv)
 
       if (user_input == "exit") break;
 
+      std::string user_command = RUBY_DEV_COMMAND;
+      user_command = __replace(user_command, "[[COMPONENT_NAME]]", user_input);
+
       std::vector<std::string> command_tokens;
       command_tokens = {
          "(",
             "cd /Users/markoates/dev_repos/partners",
             "&&",
-            RUBY_DEV_COMMAND,
+            "(",
+               user_command,
+            ")",
          ")",
       };
 
