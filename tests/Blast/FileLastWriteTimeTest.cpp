913d66da3a7e1d6ae83514efa6804433cfefb6d5
Mark Oates <markoates0@gmail.com>
2020-03-02 22:14:28 -0500

    Add Blast/FileLastWriteTime component with tests

---
+++ tests/Blast/FileLastWriteTimeTest.cpp
@@ -0,0 +1,19 @@
+
+#include <gtest/gtest.h>
+
+#include <Blast/FileLastWriteTime.hpp>
+
+TEST(Blast_FileLastWriteTimeTest, can_be_created_without_blowing_up)
+{
+   Blast::FileLastWriteTime file_last_write_time;
+}
+
+TEST(Blast_FileLastWriteTimeTest, last_write_time__returns_the_last_write_time_of_the_file)
+{
+   std::string filename = "tests/fixtures/file_that_should_not_change_modified_time.txt";
+   Blast::FileLastWriteTime file_last_write_time(filename);
+   std::time_t expected_write_time = 1583205122;
+   std::time_t actual_write_time = file_last_write_time.last_write_time();
+   EXPECT_EQ(expected_write_time, actual_write_time);
+}
+
