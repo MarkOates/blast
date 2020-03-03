b8a6b7b1ec5ae68e7cbce4b00b38870944132b07
Mark Oates <markoates0@gmail.com>
2020-03-02 22:40:04 -0500

    Add disabled test on Blast/FileLastWriteTime to indicate throwing behavior

--- tests/Blast/FileLastWriteTimeTest.cpp
+++ tests/Blast/FileLastWriteTimeTest.cpp
@@ -8,6 +8,13 @@ TEST(Blast_FileLastWriteTimeTest, can_be_created_without_blowing_up)
    Blast::FileLastWriteTime file_last_write_time;
 }
 
+TEST(DISABLED_Blast_FileLastWriteTimeTest, last_write_time__with_a_filename_that_does_not_exist__raises_an_exception)
+{
+   std::string filename = "file_that_does_not_exist.txt";
+   Blast::FileLastWriteTime file_last_write_time(filename);
+   file_last_write_time.last_write_time();
+}
+
 TEST(Blast_FileLastWriteTimeTest, last_write_time__returns_the_last_write_time_of_the_file)
 {
    std::string filename = "tests/fixtures/file_that_should_not_change_modified_time.txt";
