72d99c240dab648d6505ba6355958516f4eed3c4
Mark Oates <markoates0@gmail.com>
2020-03-08 12:35:11 -0400

    Remove usage of "__fs" prefix for experimental lib usage; Update to non-experimental

--- src/Blast/FileLastWriteTime.cpp
+++ src/Blast/FileLastWriteTime.cpp
@@ -22,7 +22,7 @@ FileLastWriteTime::~FileLastWriteTime()
 
 std::time_t FileLastWriteTime::last_write_time()
 {
-auto ftime = std::__fs::filesystem::last_write_time(filename);
+auto ftime = std::filesystem::last_write_time(filename);
 std::time_t last_write_time = decltype(ftime)::clock::to_time_t(ftime);
 return last_write_time;
 
