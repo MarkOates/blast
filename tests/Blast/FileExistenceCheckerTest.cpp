985299cff5fe014925084de35b960dbd614604b1
Mark Oates <markoates0@gmail.com>
2020-03-08 10:58:25 -0400

    Add disabled tests for FileExistenceChecker specs

--- tests/Blast/FileExistenceCheckerTest.cpp
+++ tests/Blast/FileExistenceCheckerTest.cpp
@@ -13,3 +13,15 @@ TEST(Blast_FileExistenceCheckerTest, run__returns_the_expected_response)
    Blast::FileExistenceChecker file_existence_checker("foobar");
    EXPECT_EQ(false, file_existence_checker.exists());
 }
+
+TEST(DISABLED_Blast_FileExistenceCheckerTest, exists__returns_true_for_regular_files)
+{
+}
+
+TEST(DISABLED_Blast_FileExistenceCheckerTest, exists__returns_false_for_directories)
+{
+}
+
+TEST(DISABLED_Blast_FileExistenceCheckerTest, exists__returns_true_for_symlinks)
+{
+}
