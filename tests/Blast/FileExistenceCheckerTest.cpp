4304b08a9c7808f6673922446222a7e5deb00d23
Mark Oates <markoates0@gmail.com>
2020-03-08 12:33:39 -0400

    Remove legacy implementation in FileExistenceChecker; Use <filesystem> finally!

--- tests/Blast/FileExistenceCheckerTest.cpp
+++ tests/Blast/FileExistenceCheckerTest.cpp
@@ -14,8 +14,13 @@ TEST(Blast_FileExistenceCheckerTest, run__returns_the_expected_response)
    EXPECT_EQ(false, file_existence_checker.exists());
 }
 
-TEST(DISABLED_Blast_FileExistenceCheckerTest, exists__returns_true_for_regular_files)
+TEST(Blast_FileExistenceCheckerTest, exists__returns_true_for_regular_files)
 {
+   std::string test_fixtures_directory = "/Users/markoates/Repos/blast/bin/fixtures/";
+   std::string expected_file_to_exist = test_fixtures_directory + "file_that_exists.txt";
+
+   Blast::FileExistenceChecker file_existence_checker(expected_file_to_exist);
+   EXPECT_EQ(true, file_existence_checker.exists());
 }
 
 TEST(DISABLED_Blast_FileExistenceCheckerTest, exists__returns_false_for_directories)
