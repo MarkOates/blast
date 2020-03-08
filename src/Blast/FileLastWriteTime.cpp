83c10dbb0317f922fbc751ae903c1e6d3f39597d
Mark Oates <markoates0@gmail.com>
2020-03-08 12:58:15 -0400

    Fix "_WIN22" typo to "_WIN32"

--- src/Blast/FileLastWriteTime.cpp
+++ src/Blast/FileLastWriteTime.cpp
@@ -23,7 +23,7 @@ FileLastWriteTime::~FileLastWriteTime()
 std::time_t FileLastWriteTime::last_write_time()
 {
 auto ftime = std::filesystem::last_write_time(filename);
-#ifdef _WIN22
+#ifdef _WIN32
   return ftime; // will fail to compile
 #else
   std::time_t last_write_time = decltype(ftime)::clock::to_time_t(ftime);
