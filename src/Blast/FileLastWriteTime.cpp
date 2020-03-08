53459c86f7a889e05705c184d30da21a879019e3
Mark Oates <markoates0@gmail.com>
2020-03-08 13:33:49 -0400

    Use unideal (but cross-compatible) implemention in Blast/FileLastWriteTime

--- src/Blast/FileLastWriteTime.cpp
+++ src/Blast/FileLastWriteTime.cpp
@@ -23,12 +23,12 @@ FileLastWriteTime::~FileLastWriteTime()
 std::time_t FileLastWriteTime::last_write_time()
 {
 auto ftime = std::filesystem::last_write_time(filename);
-#ifdef _WIN32
-  return ftime; // will fail to compile
-#else
-  std::time_t last_write_time = decltype(ftime)::clock::to_time_t(ftime);
-  return last_write_time;
-#endif
+auto sctp = std::chrono::time_point_cast<std::chrono::system_clock::duration>(ftime - std::filesystem::file_time_type::clock::now() + std::chrono::system_clock::now());
+return std::chrono::system_clock::to_time_t(sctp);
+
+// below is the preferred implementation, but is not supported until c++20
+//std::time_t last_write_time = decltype(ftime)::clock::to_time_t(ftime);
+//return last_write_time;
 
 }
 } // namespace Blast
