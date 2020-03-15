06040434402e0011abc96e990aa6571586372410
Mark Oates <markoates0@gmail.com>
2020-03-15 13:37:56 -0400

    Rename test in FileExistenceChecker and move down

--- tests/Blast/FileExistenceCheckerTest.cpp
+++ tests/Blast/FileExistenceCheckerTest.cpp
@@ -8,12 +8,6 @@ TEST(Blast_FileExistenceCheckerTest, can_be_created_without_blowing_up)
    Blast::FileExistenceChecker file_existence_checker;
 }
 
-TEST(Blast_FileExistenceCheckerTest, run__returns_the_expected_response)
-{
-   Blast::FileExistenceChecker file_existence_checker("foobar");
-   EXPECT_EQ(false, file_existence_checker.exists());
-}
-
 TEST(Blast_FileExistenceCheckerTest, exists__returns_true_for_regular_files)
 {
    std::string test_fixtures_directory = "/Users/markoates/Repos/blast/bin/fixtures/";
@@ -21,6 +15,12 @@ TEST(Blast_FileExistenceCheckerTest, exists__returns_true_for_regular_files)
 
    Blast::FileExistenceChecker file_existence_checker(expected_file_to_exist);
    EXPECT_EQ(true, file_existence_checker.exists());
+}
+
+TEST(Blast_FileExistenceCheckerTest, exists__returns_false_for_files_that_are_missing)
+{
+   Blast::FileExistenceChecker file_existence_checker("file_that_does_not_exist.txt");
+   EXPECT_EQ(false, file_existence_checker.exists());
 }
 
 TEST(DISABLED_Blast_FileExistenceCheckerTest, exists__returns_false_for_directories)
