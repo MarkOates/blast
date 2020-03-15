4a954d1998a2075d49e59077a691ca00c283a719
Mark Oates <markoates0@gmail.com>
2020-03-15 13:31:11 -0400

    Include absolute path inclusion in Project/Component test

--- tests/Blast/Project/ComponentTest.cpp
+++ tests/Blast/Project/ComponentTest.cpp
@@ -31,56 +31,55 @@ TEST(Blast_Project_ComponentTest, generate_full_path_test_binary_filename__retur
 
 TEST(Blast_Project_ComponentTest, exists__returns_true_if_any_component_files_are_present)
 {
-   // this may require a more robust test
-   Blast::Project::Component component("Blast/Project/Component");
+   Blast::Project::Component component("Blast/Project/Component", ABSOLUTE_PATH(""));
    ASSERT_EQ(true, component.exists());
 }
 
 TEST(Blast_Project_ComponentTest, exists__returns_false_if_no_component_files_are_present)
 {
-   Blast::Project::Component component("Blast/ComponentThatDoesNotExist");
+   Blast::Project::Component component("Blast/ComponentThatDoesNotExist", ABSOLUTE_PATH(""));
    ASSERT_EQ(false, component.exists());
 }
 
 TEST(Blast_Project_ComponentTest, has_quintessence__returns_true_when_a_quintessence_file_is_present)
 {
-   Blast::Project::Component component("Blast/Project/Component");
+   Blast::Project::Component component("Blast/Project/Component", ABSOLUTE_PATH(""));
    ASSERT_EQ(true, component.has_quintessence());
 }
 
 TEST(Blast_Project_ComponentTest, has_quintessence__returns_false_when_a_quintessence_file_is_not_present)
 {
-   Blast::Project::Component component("Blast/DiceRoller");
+   Blast::Project::Component component("Blast/DiceRoller", ABSOLUTE_PATH(""));
    ASSERT_EQ(false, component.has_quintessence());
 }
 
 TEST(Blast_Project_ComponentTest, has_only_source_and_header__returns_true_when_the_source_and_header_files_are_present_and_a_quintessence_file_does_not_exist)
 {
-   Blast::Project::Component component("Blast/DiceRoller");
+   Blast::Project::Component component("Blast/DiceRoller", ABSOLUTE_PATH(""));
    ASSERT_EQ(true, component.has_only_source_and_header());
 }
 
 TEST(Blast_Project_ComponentTest, has_only_source_and_header__returns_false_when_a_quintessence_file_is_present)
 {
-   Blast::Project::Component component("Blast/Projets/Component");
+   Blast::Project::Component component("Blast/Projets/Component", ABSOLUTE_PATH(""));
    ASSERT_EQ(false, component.has_only_source_and_header());
 }
 
 TEST(Blast_Project_ComponentTest, has_test__returns_true_when_a_file_exists)
 {
-   Blast::Project::Component component("Blast/TemplatedFile");
+   Blast::Project::Component component("Blast/TemplatedFile", ABSOLUTE_PATH(""));
    ASSERT_EQ(true, component.has_test());
 }
 
 TEST(Blast_Project_ComponentTest, has_test__returns_false_when_a_test_file_does_not_exist)
 {
-   Blast::Project::Component component("Blast/StringSplitter");
+   Blast::Project::Component component("Blast/StringSplitter", ABSOLUTE_PATH(""));
    ASSERT_EQ(false, component.has_test());
 }
 
 TEST(Blast_Project_ComponentTest, last_write_time__returns_the_most_recent_write_time_among_all_of_the_component_files)
 {
-   Blast::Project::Component component("Blast/StringSplitter");
+   Blast::Project::Component component("Blast/StringSplitter", ABSOLUTE_PATH(""));
    std::time_t actual_last_write_time = component.last_write_time();
    ASSERT_GT(actual_last_write_time, 0);
 
@@ -91,7 +90,7 @@ TEST(Blast_Project_ComponentTest, last_write_time__returns_the_most_recent_write
 
 TEST(Blast_Project_ComponentTest, last_write_time__returns_0_when_the_component_does_not_exist)
 {
-   Blast::Project::Component component("Blast/Component/That/Does/Not/Exist");
+   Blast::Project::Component component("Blast/Component/That/Does/Not/Exist", ABSOLUTE_PATH(""));
    ASSERT_FALSE(component.exists());
    std::time_t actual_write_time = component.last_write_time();
    ASSERT_EQ(0, actual_write_time);
