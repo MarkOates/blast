24eb75f7778ea1418c831cd6e9c42c21438b1727
Mark Oates <markoates0@gmail.com>
2020-03-15 23:08:47 -0400

    Add another test for Blast/RegexMatcher

--- tests/Blast/RegexMatcherTest.cpp
+++ tests/Blast/RegexMatcherTest.cpp
@@ -16,3 +16,12 @@ TEST(Blast_RegexMatcherTest, get_match_info__returns_a_list_of_matches__test_1)
 
    EXPECT_EQ(expected_matches, actual_matches);
 }
+
+TEST(Blast_RegexMatcherTest, get_match_info__returns_a_list_of_matches__test_2)
+{
+   Blast::RegexMatcher regex_matcher("Hello World!", "o");
+   std::vector<std::pair<int, int>> expected_matches = { { 4, 1 }, { 7, 1 } };
+   std::vector<std::pair<int, int>> actual_matches = regex_matcher.get_match_info();
+
+   EXPECT_EQ(expected_matches, actual_matches);
+}
