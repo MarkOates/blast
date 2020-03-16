0d78cfea5565eac1eaa6600a1eaa02a9d0d6ef8a
Mark Oates <markoates0@gmail.com>
2020-03-15 23:07:25 -0400

    Add implementation for Blast/RegexMatcher

--- src/Blast/RegexMatcher.cpp
+++ src/Blast/RegexMatcher.cpp
@@ -1,7 +1,11 @@
 
 
 #include <Blast/RegexMatcher.hpp>
-
+#include <regex>
+#include <regex>
+#include <regex>
+#include <regex>
+#include <regex>
 
 
 namespace Blast
@@ -34,7 +38,31 @@ std::string RegexMatcher::get_regex_expression()
 
 std::vector<std::pair<int, int>> RegexMatcher::get_match_info()
 {
-return {};
+std::vector<std::pair<int, int>> results;
+
+std::string subject(source_string);
+try
+{
+   //std::regex re("\\w+"); // find words
+   std::regex re(regex_expression);
+   std::sregex_iterator next(subject.begin(), subject.end(), re);
+   std::sregex_iterator end;
+   while (next != end)
+   {
+      std::smatch match = *next;
+      //std::cout << " - string: " << match.str() << std::endl;
+      //std::cout << "   position: " << match.position() << std::endl;
+      //std::cout << "   length: " << match.str().size() << std::endl;
+      results.push_back(std::pair<int, int>(match.position(), match.str().size()));
+      next++;
+   }
+}
+catch (std::regex_error& e)
+{
+   throw std::runtime_error("There was a syntax error in the regular expression");
+}
+
+return results;
 
 }
 } // namespace Blast
--- tests/Blast/RegexMatcherTest.cpp
+++ tests/Blast/RegexMatcherTest.cpp
@@ -10,8 +10,8 @@ TEST(Blast_RegexMatcherTest, can_be_created_without_blowing_up)
 
 TEST(Blast_RegexMatcherTest, get_match_info__returns_a_list_of_matches__test_1)
 {
-   Blast::RegexMatcher regex_matcher("Hello World!", "Hello");
-   std::vector<std::pair<int, int>> expected_matches = {};
+   Blast::RegexMatcher regex_matcher("Hello World!", "World");
+   std::vector<std::pair<int, int>> expected_matches = { { 6, 5 } };
    std::vector<std::pair<int, int>> actual_matches = regex_matcher.get_match_info();
 
    EXPECT_EQ(expected_matches, actual_matches);
