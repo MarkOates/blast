e21a5da4721c73b99065f760e7a4395eeb27d2ec
Mark Oates <markoates0@gmail.com>
2020-04-03 17:41:38 -0400

    Add (basically empty) StringSplitter test

---
+++ tests/Blast/StringSplitterTest.cpp
@@ -0,0 +1,12 @@
+
+
+#include <gtest/gtest.h>
+
+#include <Blast/StringSplitter.hpp>
+
+
+TEST(StringSplitterTest, can_be_created_without_blowing_up)
+{
+   Blast::StringSplitter string_splitter;
+}
+
