0a600d02945a2ecd196029f1578cfb70260475df
Mark Oates <markoates0@gmail.com>
2020-04-05 14:10:51 -0400

    Add basic interface for Daemus/ProgramRunner

---
+++ include/Blast/Daemus/ProgramRunner.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+
+
+
+namespace Blast
+{
+   namespace Daemus
+   {
+      class ProgramRunner
+      {
+      private:
+
+      public:
+         ProgramRunner();
+         ~ProgramRunner();
+
+
+      void execute_command();
+      };
+   }
+}
+
+
+
---
+++ src/Blast/Daemus/ProgramRunner.cpp
@@ -0,0 +1,31 @@
+
+
+#include <Blast/Daemus/ProgramRunner.hpp>
+
+
+
+namespace Blast
+{
+namespace Daemus
+{
+
+
+ProgramRunner::ProgramRunner()
+{
+}
+
+
+ProgramRunner::~ProgramRunner()
+{
+}
+
+
+void ProgramRunner::execute_command()
+{
+return;
+
+}
+} // namespace Daemus
+} // namespace Blast
+
+
