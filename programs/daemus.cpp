f0568d7a460bdcf6e1407b3a9f7811c21a103ae8
Mark Oates <markoates0@gmail.com>
2020-04-05 19:38:33 -0400

    Remove all Daemus code

--- programs/daemus.cpp
+++
@@ -1,9 +0,0 @@
-#include <allegro5/allegro.h> // for compatibility with union/Makefile
-#include <Blast/Daemus/ProgramRunner.hpp>
-
-int main(int argc, char **argv)
-{
-   Blast::Daemus::ProgramRunner().run();
-   return 0;
-}
-
