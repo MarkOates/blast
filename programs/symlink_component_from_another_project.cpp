1b776dc0a31f85b4744df060793b730ddd5a48ba
Mark Oates <markoates0@gmail.com>
2020-04-18 17:08:06 -0400

    Fix typo in debug text

--- programs/symlink_component_from_another_project.cpp
+++ programs/symlink_component_from_another_project.cpp
@@ -11,7 +11,7 @@ std::vector<std::string> args;
 int main(int argc, char **argv)
 {
    for (int i=0; i<argc; i++) args.push_back(argv[i]);
-   if (args.size() != 3) throw std::runtime_error("You must pass a target project name, follwed by a component name");
+   if (args.size() != 3) throw std::runtime_error("You must pass a target project name, followed by a component name.");
 
    const std::string COMMAND_TEMPLATE = "TARGET_PROJECT=[[TARGET_PROJECT]]; COMPONENT=[[COMPONENT]]; " \
                                         "ln -s /Users/markoates/Repos/${TARGET_PROJECT}/quintessence/${COMPONENT}.q.yml " \
