00f8ece2734a5826355fd2d1914805b08801c738
Mark Oates <markoates0@gmail.com>
2020-04-22 23:40:44 -0400

    Add basic implementation for "symlink_fixer" program

--- programs/symlink_fixer.cpp
+++ programs/symlink_fixer.cpp
@@ -6,6 +6,11 @@
 #include <filesystem>
 namespace fs = std::filesystem;
 
+bool starts_with(std::string source, std::string string_to_find)
+{
+   return source.rfind(string_to_find, 0) == 0;
+}
+
 int main(int argc, char **argv)
 {
    std::vector<std::string> filenames = {};
@@ -15,7 +20,16 @@ int main(int argc, char **argv)
       {
          filenames.push_back(p.path());
          std::string symlink_target = fs::read_symlink(p);
-         std::cout << p.path().string() << " -> " << symlink_target << '\n';
+         std::string sanitized_target = symlink_target;
+         if (starts_with(symlink_target, "/Users/markoates/Repos/"))
+         {
+            sanitized_target.replace(0, std::string("/Users/markoates/Repos/").length(), "../../../");
+            std::cout << "!!!!!";
+         }
+
+         fs::remove(p.path());
+         fs::create_symlink(sanitized_target, p.path());
+         std::cout << p.path().string() << " -> " << sanitized_target << '\n';
       }
    }
 }
