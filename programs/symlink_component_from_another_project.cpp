f7bca8d3ac0a2a1d2ae2579c859e5651b38df34e
Mark Oates <markoates0@gmail.com>
2020-04-25 21:45:16 -0400

    Update "symlink_component_from_another_project", Fix number of "../" prefixing in commands

--- programs/symlink_component_from_another_project.cpp
+++ programs/symlink_component_from_another_project.cpp
@@ -2,11 +2,19 @@
 
 
 #include <iostream>
+#include <algorithm> // for std::count
 #include <Blast/TemplatedFile.hpp>
 
 
 std::vector<std::string> args;
 
+#include <string>
+std::string repeat(std::string s, int n)
+{
+   std::string s1 = s;
+   for (int i=1; i<n;i++) s += s1;
+   return s;
+}
 
 int main(int argc, char **argv)
 {
@@ -14,19 +22,19 @@ int main(int argc, char **argv)
    if (args.size() != 3) throw std::runtime_error("You must pass a target project name, followed by a component name.");
 
    const std::string COMMAND_TEMPLATE = "TARGET_PROJECT=[[TARGET_PROJECT]]; COMPONENT=[[COMPONENT]]; " \
-                                        "ln -s ../${TARGET_PROJECT}/quintessence/${COMPONENT}.q.yml " \
+                                        "ln -s [[UP_DIRECTORY_PARENT_TOKENS]]${TARGET_PROJECT}/quintessence/${COMPONENT}.q.yml " \
                                         "[[TARGET_PROJECT_FOLDER_NAME]]/quintessence/${COMPONENT}.q.yml; " \
                                         "unset COMPONENT; unset TARGET_PROJECT";
 
    const std::string TEST_FILE_COMMAND_TEMPLATE = "TARGET_PROJECT=[[TARGET_PROJECT]]; COMPONENT=[[COMPONENT]]; " \
-                                                  "ln -s ../${TARGET_PROJECT}/tests/${COMPONENT}Test.cpp " \
+                                                  "ln -s [[UP_DIRECTORY_PARENT_TOKENS]]${TARGET_PROJECT}/tests/${COMPONENT}Test.cpp " \
                                                   "[[TARGET_PROJECT_FOLDER_NAME]]/tests/${COMPONENT}Test.cpp; " \
                                                   "unset COMPONENT; unset TARGET_PROJECT";
 
    const std::string SOURCE_HEADER_COMMAND_TEMPLATE = "TARGET_PROJECT=[[TARGET_PROJECT]]; COMPONENT=[[COMPONENT]]; " \
-                                                      "ln -s ../${TARGET_PROJECT}/src/${COMPONENT}.cpp " \
+                                                      "ln -s [[UP_DIRECTORY_PARENT_TOKENS]]${TARGET_PROJECT}/src/${COMPONENT}.cpp " \
                                                       "[[TARGET_PROJECT_FOLDER_NAME]]/src/${COMPONENT}.cpp; " \
-                                                      "ln -s ../${TARGET_PROJECT}/include/${COMPONENT}.hpp " \
+                                                      "ln -s [[UP_DIRECTORY_PARENT_TOKENS]]${TARGET_PROJECT}/include/${COMPONENT}.hpp " \
                                                       "[[TARGET_PROJECT_FOLDER_NAME]]/include/${COMPONENT}.hpp; " \
                                                       "unset COMPONENT; unset TARGET_PROJECT";
 
@@ -34,22 +42,28 @@ int main(int argc, char **argv)
    std::string component_name = args[2]; //"Blast/ShellCommandExecutorWithCallback";
    std::string target_project_raw_folder_name = ".";
 
+   int component_directory_depth = (int)std::count(component_name.begin(), component_name.end(), '/') + 1;
+   std::string up_directory_parent_tokens = repeat("../", component_directory_depth);
+
    Blast::TemplatedFile templated_file(COMMAND_TEMPLATE, {
       { "[[TARGET_PROJECT]]",             source_project_raw_folder_name },
       { "[[COMPONENT]]",                  component_name },
       { "[[TARGET_PROJECT_FOLDER_NAME]]", target_project_raw_folder_name },
+      { "[[UP_DIRECTORY_PARENT_TOKENS]]", up_directory_parent_tokens },
    });
 
    Blast::TemplatedFile templated_test_command(TEST_FILE_COMMAND_TEMPLATE, {
       { "[[TARGET_PROJECT]]",             source_project_raw_folder_name },
       { "[[COMPONENT]]",                  component_name },
       { "[[TARGET_PROJECT_FOLDER_NAME]]", target_project_raw_folder_name },
+      { "[[UP_DIRECTORY_PARENT_TOKENS]]", up_directory_parent_tokens },
    });
 
    Blast::TemplatedFile templated_source_header_command(SOURCE_HEADER_COMMAND_TEMPLATE, {
       { "[[TARGET_PROJECT]]",             source_project_raw_folder_name },
       { "[[COMPONENT]]",                  component_name },
       { "[[TARGET_PROJECT_FOLDER_NAME]]", target_project_raw_folder_name },
+      { "[[UP_DIRECTORY_PARENT_TOKENS]]", up_directory_parent_tokens },
    });
 
    std::string result_command = templated_file.generate_content();
