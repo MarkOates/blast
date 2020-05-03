f31a0678b285bc35b8523daf1f8d71f29bae489e
Mark Oates <markoates0@gmail.com>
2020-05-03 12:59:19 -0400

    * Add first test for #likely_an_intended_symlink#

--- tests/Blast/Project/ProjectSymlinkFixerTest.cpp
+++ tests/Blast/Project/ProjectSymlinkFixerTest.cpp
@@ -3,6 +3,12 @@
 
 #include <Blast/Project/ProjectSymlinkFixer.hpp>
 
+#include <Blast/Project/SymlinkChecker.hpp>
+
+
+#include <filesystem>
+
+
 TEST(Blast_Project_ProjectSymlinkFixerTest, can_be_created_without_blowing_up)
 {
    Blast::Project::ProjectSymlinkFixer project_symlink_fixer;
@@ -18,6 +24,13 @@ TEST(Blast_Project_ProjectSymlinkFixerTest, project_foler__has_getter_and_is_set
 TEST(Blast_Project_ProjectSymlinkFixerTest,
    likey_an_intended_symlink__returns_true_if_the_file_is_actually_a_symlink)
 {
+   std::string filename_of_file_that_is_symlink =
+      "./bin/fixtures/FixtureProject2/quintessence/ComponentWithExternalSymlink.q.yml";
+   ASSERT_EQ(true, std::filesystem::exists(filename_of_file_that_is_symlink));
+   ASSERT_EQ(true, std::filesystem::is_symlink(filename_of_file_that_is_symlink));
+
+   Blast::Project::ProjectSymlinkFixer project_symlink_fixer;
+   ASSERT_EQ(true, project_symlink_fixer.likely_an_intended_symlink(filename_of_file_that_is_symlink));
 }
 
 TEST(Blast_Project_ProjectSymlinkFixerTest,
