d2ef8c0b1e0a13e6d1d23591d6661b6d0f08fafd
Mark Oates <markoates0@gmail.com>
2020-05-16 22:57:52 -0400

    * Add a convenience #would_be_included_in_results# for SimpleTextSearcher

--- tests/Blast/SimpleTextSearcherTest.cpp
+++ tests/Blast/SimpleTextSearcherTest.cpp
@@ -27,3 +27,27 @@ TEST(Blast_SimpleTextSearcherTest, results__returns_elements_matching_the_search
 
    EXPECT_EQ(expected_results, simple_text_searcher.results());
 }
+
+TEST(Blast_SimpleTextSearcherTest,
+   would_be_include_in_results__will_return_true_if_the_element_would_be_included_in_the_results_if_present)
+{
+   std::vector<std::string> elements = { "Foo", "Bar", "", " ", "FooBar" };
+   Blast::SimpleTextSearcher simple_text_searcher("Foo", elements);
+
+   EXPECT_EQ(true, simple_text_searcher.would_be_included_in_results("Foo"));
+   EXPECT_EQ(true, simple_text_searcher.would_be_included_in_results("FooBar"));
+   EXPECT_EQ(true, simple_text_searcher.would_be_included_in_results("FooBarFlex"));
+}
+
+TEST(Blast_SimpleTextSearcherTest,
+   would_be_include_in_results__will_return_false_if_the_element_would_not_be_included_in_the_results_if_present)
+{
+   std::vector<std::string> elements = { "Foo", "Bar", "", " ", "FooBar" };
+   Blast::SimpleTextSearcher simple_text_searcher("Foo", elements);
+
+   EXPECT_EQ(false, simple_text_searcher.would_be_included_in_results("NonMatching"));
+   EXPECT_EQ(false, simple_text_searcher.would_be_included_in_results("Bar"));
+   EXPECT_EQ(false, simple_text_searcher.would_be_included_in_results(""));
+   EXPECT_EQ(false, simple_text_searcher.would_be_included_in_results(" "));
+}
+
