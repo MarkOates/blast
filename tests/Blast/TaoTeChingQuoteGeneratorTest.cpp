08575886d9bbc124e48dd8ee3b0b8773ea9b8615
Mark Oates <markoates0@gmail.com>
2020-07-01 16:03:59 -0400

    * Add test to check expected returned quote

--- tests/Blast/TaoTeChingQuoteGeneratorTest.cpp
+++ tests/Blast/TaoTeChingQuoteGeneratorTest.cpp
@@ -3,6 +3,12 @@
 
 #include <Blast/TaoTeChingQuoteGenerator.hpp>
 
+#include <gmock/gmock.h>
+using ::testing::AnyOfArray;
+
+#include <gmock/gmock.h>
+using ::testing::IsSupersetOf;
+
 TEST(Blast_TaoTeChingQuoteGeneratorTest, can_be_created_without_blowing_up)
 {
    Blast::TaoTeChingQuoteGenerator tao_te_ching_quote_generator;
@@ -21,4 +27,20 @@ TEST(Blast_TaoTeChingQuoteGeneratorTest, pick_quote__with_an_empty_list__returns
    Blast::TaoTeChingQuoteGenerator tao_te_ching_quote_generator(empty_list_of_quotes);
    std::string expected_empty_string = "";
    ASSERT_EQ(expected_empty_string, tao_te_ching_quote_generator.pick_quote());
+}
+
+TEST(Blast_TaoTeChingQuoteGeneratorTest, pick_quote__will_select_a_random_quote_from_the_list_of_provided_quotes)
+{
+   std::vector<std::string> list_of_test_quotes = {
+      "Darkness within darkness. The gateway to all understanding.",
+      "It is hidden but always present. I don't know who gave birth to it. It is older than God.",
+      "The greatest art seems unsophisticated, the greatest love seems indifferent, the greatest wisdom seems childish.",
+   };
+   Blast::TaoTeChingQuoteGenerator tao_te_ching_quote_generator(list_of_test_quotes);
+
+   for (unsigned i=0; i<list_of_test_quotes.size(); i++)
+   {
+      std::string returned_quote = tao_te_ching_quote_generator.pick_quote();
+      EXPECT_THAT(returned_quote, AnyOfArray(list_of_test_quotes));
+   }
 }
