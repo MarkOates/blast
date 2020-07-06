974b1e23149e2bd9a966c98f7e0a7b1c1569ebb3
Mark Oates <markoates0@gmail.com>
2020-07-05 20:58:05 -0400

    * Add some extra test coverage to the Blast/DirectoryExistenceChecker

--- tests/Blast/DirectoryExistenceCheckerTest.cpp
+++ tests/Blast/DirectoryExistenceCheckerTest.cpp
@@ -11,7 +11,7 @@ TEST(DirectoryExistenceCheckerTest, can_be_created_without_arguments)
 }
 
 
-TEST(DirectoryExistenceCheckerTest, exists_returns_true_when_a_directory_is_present)
+TEST(DirectoryExistenceCheckerTest, exists__returns_true_when_a_directory_is_present)
 {
    std::string directory_that_should_exist = "quintessence"; // this directory is expected to exist on the filesystem
                                                              // note that this test executable must be run from the
@@ -21,9 +21,24 @@ TEST(DirectoryExistenceCheckerTest, exists_returns_true_when_a_directory_is_pres
 }
 
 
-TEST(DirectoryExistenceCheckerTest, exists_returns_false_when_a_directory_is_not_present)
+TEST(DirectoryExistenceCheckerTest, exists__returns_false_when_a_directory_is_not_present)
 {
    Blast::DirectoryExistenceChecker directory_existence_checker("directory_that_does_not_exist");
+   ASSERT_FALSE(directory_existence_checker.exists());
+}
+
+
+TEST(DirectoryExistenceCheckerTest, exists__without_arguments__does_not_blow_up)
+{
+   Blast::DirectoryExistenceChecker directory_existence_checker;
+   directory_existence_checker.exists();
+   SUCCEED();
+}
+
+
+TEST(DirectoryExistenceCheckerTest, exists__with_an_empty_string_directory__returns_false)
+{
+   Blast::DirectoryExistenceChecker directory_existence_checker("");
    ASSERT_FALSE(directory_existence_checker.exists());
 }
 
