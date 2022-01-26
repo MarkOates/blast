91864ce54c9bcc2eb26ccb1aaaeedcde68c9f45c
Mark Oates <markoates0@gmail.com>
2022-01-25 22:28:45 -0500

    Move created vars to top of operation

--- programs/component_generator.cpp
+++ programs/component_generator.cpp
@@ -184,6 +184,15 @@ int main(int argc, char **argv)
    // create the component generator
    ComponentGenerator generator(argv[1]);
 
+   // create the template variable and eplacement set
+   std::vector<std::pair<std::string, std::string>> template_var_and_replacement_set = {
+      { "[[COMPONENT_HEADER_INCLUDE_FILE_PATH]]", generator.get_header_filename() },
+      { "[[COMPONENT_TEST_DESCRIPTION_NAME]]", generator.get_google_test_description_prefix() },
+      { "[[COMPONENT_CLASS_NAME]]", generator.get_program_body_class_name() },
+      { "[[COMPONENT_BASENAME_SNAKE_CASE]]", generator.get_component_tail_snakecase() },
+      { "[[COMPONENT_AS_ALL_CAPS_CONSTANT]]", generator.get_component_tail_all_caps_constant() },
+   };
+
    // create the folders for the components
    std::cout << "Making sure necessary folders are present...";
    create_directory(generator.get_quintessence_foldername());
@@ -222,15 +231,6 @@ int main(int argc, char **argv)
       error_message << outfiles_that_cannot_be_opened.str();
       throw std::runtime_error(error_message.str());
    }
-
-   // create the template variable and eplacement set
-   std::vector<std::pair<std::string, std::string>> template_var_and_replacement_set = {
-      { "[[COMPONENT_HEADER_INCLUDE_FILE_PATH]]", generator.get_header_filename() },
-      { "[[COMPONENT_TEST_DESCRIPTION_NAME]]", generator.get_google_test_description_prefix() },
-      { "[[COMPONENT_CLASS_NAME]]", generator.get_program_body_class_name() },
-      { "[[COMPONENT_BASENAME_SNAKE_CASE]]", generator.get_component_tail_snakecase() },
-      { "[[COMPONENT_AS_ALL_CAPS_CONSTANT]]", generator.get_component_tail_all_caps_constant() },
-   };
 
    // write the quintessence file content to the file and close it (the file currently has not templated replacement strings)
    Blast::TemplatedFile templated_quintessence_file(QUINTESSENCE_FILE_CONTENT, template_var_and_replacement_set);
