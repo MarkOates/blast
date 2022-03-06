52ae3d04dd3b02aa38f07e4508ff06086187152b
Mark Oates <markoates0@gmail.com>
2022-03-06 16:13:09 -0500

    Output command to chnge the symbol when renaming a component

--- programs/component_renamer.cpp
+++ programs/component_renamer.cpp
@@ -4,6 +4,9 @@
 #include <string>
 
 
+#include <Blast/Quintessence/ComponentGenerator.hpp>
+
+
 std::string build_help_error_message()
 {
    return R"(
@@ -20,12 +23,14 @@ Important:
 
 Note that this is a very dangerous proceedure and can easily mangle your codebase if not done
 properly. Running this program will NOT execute any side effects, but will provide the commands
-you need to execute the steps yourself. For help with the process, view Hexagon's documentation
-under "Proceedure for Renaming a Component":
+you need to execute the steps yourself. Be sure that you are running the command(s) from within
+the project where you want to rename the component.  Make sure your working tree is clean in case
+something goes wrong and you need to reset.  For help with the process, view Hexagon's
+documentation "Proceedure for Renaming a Component":
 
 https://github.com/MarkOates/hexagon/blob/master/documentation/rename_component_proceedure.md
 
-   )";
+)";
 };
 
 
@@ -39,7 +44,32 @@ int main(int argc, char **argv)
    for (int i=1; i<argc; i++) args.push_back(argv[i]);
 
    // validate invalid number of args
-   if (args.size() != 2) { std::cout << build_help_error_message(); return 1; }
+   if (args.size() != 2)
+   {
+      std::cout << build_help_error_message();
+      return 1;
+   }
+
+   current_component_name = args[0];
+   new_component_name = args[1];
+
+   ComponentGenerator current_component_generator(current_component_name);
+   ComponentGenerator new_component_generator(new_component_name);
+
+   std::cout << std::endl;
+   std::cout << "You are replacing \"" << current_component_name << "\" with \"" << new_component_name << "\"" << std::endl;
+   std::cout << std::endl;
+
+   // rename the symbols
+   std::string current_symbol_name = current_component_generator.get_program_body_class_name();
+   std::string new_symbol_name = new_component_generator.get_program_body_class_name();
+   std::cout << "Replace the symbol recursively in all git-tracked files:" << std::endl;
+   std::cout << "========================================================" << std::endl;
+   std::cout << std::endl;
+   //std::cout << "git grep -lz 'Wicked::Zones::Base3D' | xargs -0 perl -i'' -pE \"s/Wicked::Zones::Base3D/Wicked::Entities::Zones::Base3D/g\"" << std::endl;
+   std::cout << "$ git grep -lz '" << current_symbol_name << "' | xargs -0 perl -i'' -pE \"s/" << current_symbol_name << "/" << new_symbol_name << "/g\"" << std::endl;
 
    return 0;
 }
+
+
