647b4f43a6a0c51de3b7e51bb1f89c68e30c4b07
Mark Oates <markoates0@gmail.com>
2022-09-02 17:54:29 -0400

    * Remove some extra spaces

--- tests/ncurses_art/Element/ElementBaseTest.cpp
+++ tests/ncurses_art/Element/ElementBaseTest.cpp
@@ -14,13 +14,13 @@ TEST(ElementBaseTest, derived_classes_can_be_created)
    DerivedElement derived_element;
 }
 
-TEST (ElementBaseTest, initializes_with_a_default_name_of_empty_string)
+TEST(ElementBaseTest, initializes_with_a_default_name_of_empty_string)
 {
    DerivedElement derived_element;
    ASSERT_EQ("", derived_element.get_name());
 }
 
-TEST (ElementBaseTest, can_set_and_get_the_name)
+TEST(ElementBaseTest, can_set_and_get_the_name)
 {
    DerivedElement derived_element;
 
@@ -34,32 +34,32 @@ TEST (ElementBaseTest, can_set_and_get_the_name)
    ASSERT_EQ("baz", derived_element.get_name());
 }
 
-TEST (ElementBaseTest, can_get_the_type)
+TEST(ElementBaseTest, can_get_the_type)
 {
    DerivedElement derived_element;
    ASSERT_EQ("DerivedElement", derived_element.get_type());
 }
 
-TEST (ElementBaseTest, returns_true_if_type_matches)
+TEST(ElementBaseTest, returns_true_if_type_matches)
 {
    DerivedElement derived_element;
    ASSERT_TRUE(derived_element.is_type("DerivedElement"));
 }
 
-TEST (ElementBaseTest, returns_false_if_type_matches)
+TEST(ElementBaseTest, returns_false_if_type_matches)
 {
    DerivedElement derived_element;
    ASSERT_FALSE(derived_element.is_type("NotTheCorrectElementName"));
 }
 
-TEST (ElementBaseTest, returns_true_if_name_matches)
+TEST(ElementBaseTest, returns_true_if_name_matches)
 {
    DerivedElement derived_element;
    ASSERT_TRUE(derived_element.set_name("foo"));
    ASSERT_TRUE(derived_element.is_name("foo"));
 }
 
-TEST (ElementBaseTest, returns_false_if_name_matches)
+TEST(ElementBaseTest, returns_false_if_name_matches)
 {
    DerivedElement derived_element;
    ASSERT_TRUE(derived_element.set_name("foo"));
