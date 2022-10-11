2fc1291e5024fd92d876f32db886e8f1a5bfe7a0
Mark Oates <markoates0@gmail.com>
2022-10-11 16:37:59 -0400

    Include the '\n' character as character to remove in Atomizer

--- src/Blast/DependencySymbolAtomizer.cpp
+++ src/Blast/DependencySymbolAtomizer.cpp
@@ -36,7 +36,7 @@ std::string DependencySymbolAtomizer::get_dependency_symbol() const
 std::vector<std::string> DependencySymbolAtomizer::atomize()
 {
    std::string possibly_composite_dep = dependency_symbol;
-   std::vector<char> chars_to_replace = { ',', '>', '<', '&', '*', '(', ')' };
+   std::vector<char> chars_to_replace = { ',', '>', '<', '&', '*', '(', ')', '\n' };
 
    // replace the chars
    for (auto &char_to_replace : chars_to_replace)
