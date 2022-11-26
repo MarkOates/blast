59bcdc7a605b17b78650909fc832a3f066b8cea1
Mark Oates <markoates0@gmail.com>
2022-11-26 11:36:42 -0500

    Add "runner" template to component_generator

--- programs/component_generator.cpp
+++ programs/component_generator.cpp
@@ -129,6 +129,7 @@ int main(int argc, char **argv)
    std::map<std::string, TemplateSetBase*> dictionary = {
       { "standard_component", new QuintessenceTestTemplatePair(file_get_contents(TEMPLATES_PATH + "standard.q.txt"), file_get_contents(TEMPLATES_PATH + "standard_test.txt")) },
       { "hexagon_stage", new QuintessenceTestTemplatePair(file_get_contents(TEMPLATES_PATH + "Hexagon/stage.q.txt"), file_get_contents(TEMPLATES_PATH + "Hexagon/stage_test.txt")) },
+      { "runner", new QuintessenceTestTemplatePair(file_get_contents(TEMPLATES_PATH + "runner.q.txt"), file_get_contents(TEMPLATES_PATH + "runner_test.txt")) },
       { "renderer", new QuintessenceTestTemplatePair(file_get_contents(TEMPLATES_PATH + "renderer.q.txt"), file_get_contents(TEMPLATES_PATH + "renderer_test.txt")) },
       { "base", new QuintessenceTestTemplatePair(file_get_contents(TEMPLATES_PATH + "base.q.txt"), file_get_contents(TEMPLATES_PATH + "base_test.txt")) },
       { "derived", new QuintessenceTestTemplatePair(file_get_contents(TEMPLATES_PATH + "derived.q.txt"), file_get_contents(TEMPLATES_PATH + "derived_test.txt")) },
