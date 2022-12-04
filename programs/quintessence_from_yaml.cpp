012cc65c150ba925e7d43e0d7cd5ac8f6d17c7f0
Mark Oates <markoates0@gmail.com>
2022-12-04 13:27:28 -0500

    Add feature to output red text to cerr in auto-generated guard message

--- programs/quintessence_from_yaml.cpp
+++ programs/quintessence_from_yaml.cpp
@@ -52,8 +52,9 @@ public:
       std::string template_content = R"END(if (!({{CONDITION}}))
 {
    std::stringstream error_message;
-   error_message << "{{CLASS_NAME}}" << "::" << "{{FUNCTION_NAME}}" << ": error: " << "{{MESSAGE}}";
-   throw std::runtime_error(error_message.str());
+   error_message << "[{{CLASS_NAME}}::{{FUNCTION_NAME}}]: error: {{MESSAGE}}.";
+   std::cerr << "\033[1;31m" << error_message.str() << " An exception will be thrown to halt the program.\033[0m" << std::endl;
+   throw std::runtime_error("{{CLASS_NAME}}::{{FUNCTION_NAME}}: error: {{MESSAGE}}");
 })END";
 
       std::vector<std::pair<std::string, std::string>> insertion_variables = {
@@ -70,7 +71,7 @@ public:
 
    static std::vector<std::string> good_enough_for_now_guards_dependency_symbols()
    {
-      return { "std::runtime_error", "std::stringstream" };
+      return { "std::runtime_error", "std::stringstream", "std::cerr" };
    }
 
    static std::string generate_guards_code(std::vector<std::string> guard_conditionals, std::string class_name, std::string function_name)
