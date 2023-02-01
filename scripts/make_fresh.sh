f7232cc2783ad85c732fd4e21fd1fdb905498372
Mark Oates <markoates0@gmail.com>
2023-02-01 17:10:49 -0500

    Add a yes/no prompt to remind of a fresh "status" before running "make_fresh.sh"

--- scripts/make_fresh.sh
+++ scripts/make_fresh.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+
+while true; do
+    read -p "For this script to run effectively, it's important that \"status\" has run clean. This is just a reminder. Do you wish to continue with the script? " yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
 cd /Users/markoates/Repos
 
 # sync dotfiles
