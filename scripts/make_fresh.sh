3daefe254295037ab4cb102653384838708a325e
Mark Oates <markoates0@gmail.com>
2023-02-02 11:47:48 -0500

    Add a more comprehensive list of projects to "make_fresh"

--- scripts/make_fresh.sh
+++ scripts/make_fresh.sh
@@ -1,5 +1,45 @@
 #!/bin/sh
 
+#----- core -----
+
+#- dotfiles
+#- union
+#- blast
+#- allegro_flare
+#- hexagon
+#- me
+
+#----- projects -----
+
+#CubeShooter
+#KrampusReturns
+#Mania
+#CatDetective
+#TrackLock
+#LightracerMax
+#LabyrinthOfLore
+#TheWeepingHouse
+#DragonWrath
+#Krampus21 (FadeToWhite)
+#Fullscore
+#Golf
+#Peri (?)
+#Solitare (?)
+#Epidemic (?)
+#AllegroMembersClub (?)
+#Releaser
+#Slug3D (slug_3d)
+#Dungeon
+#Motris
+#ChatGPT
+#beebot
+#AlexPark
+#bearys_bash
+#oatescodes
+#tileo (x)
+
+
+
 while true; do
     read -p "For this script to run effectively, it's important that \"status\" has run clean. This is just a reminder. Do you wish to continue with the script? " yn
     case $yn in
@@ -25,3 +65,5 @@ make clean && make programs -j8 && make && make clean && make programs -j8 && ma
 cd ../allegro_flare
 git pull
 make clean && make
+
+
