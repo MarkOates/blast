f580b0e7291d9b779a2c9efb87747441554cdb8e
Mark Oates <markoates0@gmail.com>
2023-02-02 11:31:36 -0500

    Modify runner template and project_generators main function to use DeploymentEnvironment

--- programs/generator.cpp
+++ programs/generator.cpp
@@ -102,10 +102,23 @@ std::string PROGRAM_RUNNER_CLASS_NAME = "ProgramRunner";
 std::string main_file_content_template = R"END(
 //#include <[[PROGRAM_RUNNER_CLASS_NAME]].hpp>
 #include <allegro5/allegro.h>
+#include <vector> // for parsing command line args
+#include <algorithm> // for parsing command line args
 
 int main(int argc, char **argv)
 {
-   //[[PROGRAM_RUNNER_CLASS_NAME]]().run();
+   std::vector<std::string> command_line_args;
+   for (int i=1; i<argc; i++) command_line_args.push_back(argv[i]);
+   bool contains_production_flag = std::count(command_line_args.begin(), command_line_args.end(), "--production");
+
+   // TODO: improve this to return an error when an unrecognized flag is present
+
+   std::string deployment_environment =
+         contains_production_flag
+         ? AllegroFlare::DeploymentEnvironment::ENVIRONMENT_PRODUCTION
+         : AllegroFlare::DeploymentEnvironment::ENVIRONMENT_DEVELOPMENT;
+
+   //[[PROGRAM_RUNNER_CLASS_NAME]]().run(deployment_environment);
    return 0;
 }
 )END";
