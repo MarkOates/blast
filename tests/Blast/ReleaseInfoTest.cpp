b039114d6ab156696f31d8fc403bb3e770264367
Mark Oates <markoates0@gmail.com>
2023-04-03 12:10:59 -0400

    * Add multiple test cases for #build_project_verison_string#

--- tests/Blast/ReleaseInfoTest.cpp
+++ tests/Blast/ReleaseInfoTest.cpp
@@ -13,10 +13,33 @@ TEST(Blast_ReleaseInfoTest, can_be_created_without_blowing_up)
 TEST(Blast_ReleaseInfoTest, build_project_version_string__will_return_a_nicely_formatted_string_of_the_verion)
 {
    Blast::ReleaseInfo release_info;
-   release_info.set_project_version(7, 12, 0);
-   std::string expected_version_string = "7.12.0";
-   std::string actual_version_string = release_info.build_project_version_string();
-   EXPECT_EQ(expected_version_string, actual_version_string);
+   std::vector<std::pair<std::tuple<int, int, int, std::vector<std::string>, std::vector<std::string>>, std::string>>
+         test_datas = {
+      { { 7,  12,  0,          {},      {}, },                     "7.12.0" },
+      { { 12,  0,  1, { "alpha" },      {}, },                     "12.0.1-alpha" },
+      { { 12,  0,  1, { "wip", "alpha" },      {}, },              "12.0.1-wip-alpha" },
+   };
+
+   for (auto &test_data : test_datas)
+   {
+      release_info.set_project_version(
+         std::get<0>(test_data.first),
+         std::get<1>(test_data.first),
+         std::get<2>(test_data.first),
+         std::get<3>(test_data.first),
+         std::get<4>(test_data.first)
+      );
+
+      std::string expected_version_string = test_data.second;
+      std::string actual_version_string = release_info.build_project_version_string();
+
+      EXPECT_EQ(expected_version_string, actual_version_string);
+   }
+
+   //release_info.set_project_version(7, 12, 0);
+   //std::string expected_version_string = "7.12.0";
+   //std::string actual_version_string = release_info.build_project_version_string();
+   //EXPECT_EQ(expected_version_string, actual_version_string);
 }
 
 
