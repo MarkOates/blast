c710dac8a0a184dca008122997d45d5f498b1d26
Mark Oates <markoates0@gmail.com>
2023-04-22 12:36:54 -0400

    * Update test to use EXPECT_STREQ for default value of string

--- tests/Blast/Cpp/FunctionBodyTest.cpp
+++ tests/Blast/Cpp/FunctionBodyTest.cpp
@@ -12,9 +12,14 @@ TEST(Blast_Cpp_FunctionBodyTest, can_be_created_without_blowing_up)
 
 TEST(Blast_Cpp_FunctionBodyTest, DEFAULT_BODY_CONTENT__has_the_expected_value)
 {
-   std::string expected_content = "return;";
-   std::string actual_content = Blast::Cpp::FunctionBody::DEFAULT_BODY_CONTENT;
-   EXPECT_EQ(expected_content, actual_content);
+   EXPECT_STREQ("return;", Blast::Cpp::FunctionBody::DEFAULT_BODY_CONTENT);
+}
+
+
+TEST(Blast_Cpp_FunctionBodyTest, content__has_the_expected_value_matching_DEFAULT_BODY_CONTENT)
+{
+   Blast::Cpp::FunctionBody function_body;
+   EXPECT_EQ(Blast::Cpp::FunctionBody::DEFAULT_BODY_CONTENT, function_body.get_content());
 }
 
 
