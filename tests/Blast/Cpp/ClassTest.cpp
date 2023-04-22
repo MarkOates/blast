cb4206a7898d83b251aaa785ce740eab3ca7b189
Mark Oates <markoates0@gmail.com>
2023-04-22 12:37:44 -0400

    * Use Testing/ErrorAssertions in Cpp/Class

--- tests/Blast/Cpp/ClassTest.cpp
+++ tests/Blast/Cpp/ClassTest.cpp
@@ -2,18 +2,12 @@
 
 #include <gtest/gtest.h>
 
+#include <Blast/Testing/ErrorAssertions.hpp>
+
 #include <Blast/Cpp/Class.hpp>
 
 #include <cmath>
 
-
-#define ASSERT_THROW_WITH_MESSAGE(code, raised_exception_type, raised_exception_message) \
-   try { code; FAIL() << "Expected " # raised_exception_type; } \
-   catch ( raised_exception_type const &err ) { EXPECT_EQ(err.what(), std::string( raised_exception_message )); } \
-   catch (...) { FAIL() << "Expected " # raised_exception_type; }
-
-
-///////
 
 
 TEST(ClassTest, can_be_created)
