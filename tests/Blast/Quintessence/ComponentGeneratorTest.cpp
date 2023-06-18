401da03240a74ee8c61248665002020449e9f11b
Mark Oates <markoates0@gmail.com>
2023-06-17 20:47:17 -0400

    * Remove unused macro

--- tests/Blast/Quintessence/ComponentGeneratorTest.cpp
+++ tests/Blast/Quintessence/ComponentGeneratorTest.cpp
@@ -5,12 +5,6 @@
 #include <Blast/Quintessence/ComponentGenerator.hpp>
 
 
-#define ASSERT_THROW_WITH_MESSAGE(code, raised_exception_type, raised_exception_message) \
-   try { code; FAIL() << "Expected " # raised_exception_type; } \
-   catch ( raised_exception_type const &err ) { EXPECT_EQ(err.what(), std::string( raised_exception_message )); } \
-   catch (...) { FAIL() << "Expected " # raised_exception_type; }
-
-
 TEST(Blast_Quintessence_ComponentGenerator, can_be_created_without_blowing_up)
 {
    ComponentGenerator generator;
