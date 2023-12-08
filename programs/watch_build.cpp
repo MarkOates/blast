449dc7bf5778e9645c6a5ab801d302dd7de605d3
Mark Oates <markoates0@gmail.com>
2023-12-07 20:37:54 -0500

    Improve "build_watch" with list of commands

--- programs/watch_build.cpp
+++ programs/watch_build.cpp
@@ -33,6 +33,31 @@ std::string timestamp_now()
 
 
 
+std::string fashion_build_command(std::string project_name, std::string command, std::string snake_cased)
+{
+   std::string build_num = "081a7bcf";
+   //std::string command = "make clean";
+   //std::string snake_cased = "make_clean";
+
+   std::string build_step_count = "001";
+
+   std::string output_folder = "/Users/markoates/builds/";
+   std::string stdout_output_filename = output_folder + build_num + "-" + build_step_count + "_" + snake_cased + "_output_stdout.txt";
+   std::string stderr_output_filename = output_folder + build_num + "-" + build_step_count + "_" + snake_cased + "_output_stderr.txt";
+
+   std::stringstream full_command_with_captures;
+   // (cd ~/Repos/"$project_name" && make clean) > make_clean_output_stdout.txt 2> make_clean_output_stderr.txt
+   full_command_with_captures << "(cd ~/Repos/" << project_name << " && " << command << ") > " << stdout_output_filename << " 2> " << stderr_output_filename;
+
+   return full_command_with_captures.str();
+}
+
+
+//(cd ~/Repos/"$project_name" && make clean && make fast && make tests && make bin/run_all_tests && make programs -j7 && make examples -j7) > output.txt 2>&1
+//exit_code=$?
+//echo "Exit code: $exit_code"
+
+
 int main(int argc, char **argv)
 {
    std::string projects_directory = "/Users/markoates/Repos/";
@@ -58,7 +83,7 @@ int main(int argc, char **argv)
    }
 
 
-   int polling_frequency_sec = 3;
+   int polling_frequency_sec = 10;
    bool abort = false;
    int project_num = 0;
 
@@ -72,6 +97,7 @@ int main(int argc, char **argv)
       std::cout << "...polling finished." << std::endl;
       std::cout << "Polled at: " << timestamp_now() << std::endl;
       std::cout << "  In sync: " << fetcher.is_the_repo_in_sync_with_remote() << std::endl;
+      bool show_message = false;
 
       if (!fetcher.is_the_repo_in_sync_with_remote())
       {
@@ -89,13 +115,31 @@ int main(int argc, char **argv)
             std::cout << "++ Pulling" << std::endl;
             std::string pull_result = Blast::String::Trimmer(fetcher.execute_command(fetcher.get_pull_command())).trim();
             std::cout << "++ Assuming pull worked fine, running a \"make clean && make\"" << std::endl;
-            std::string make_shell_command = "(cd ~/Repos/" + project_name + " && make clean && make fast && make tests && make bin/run_all_tests && make programs -j7 && make examples -j7)";
-            std::string make_shell_result = fetcher.execute_command(make_shell_command);
-            std::cout << celebrator.generate_synced_banner() << std::endl;
+
+            std::vector<std::string> commands = {
+               fashion_build_command(project_name, "make clean", "make_clean"),
+               fashion_build_command(project_name, "make fast", "make_fast"),
+               fashion_build_command(project_name, "make tests", "make_tests"),
+               fashion_build_command(project_name, "make bin/run_all_tests", "make_bin_run_all_tests"),
+               fashion_build_command(project_name, "make programs -j7", "make_programs"),
+               fashion_build_command(project_name, "make examples -j7", "make_examples"),
+            };
+
+            //std::string make_shell_command = "(cd ~/Repos/" + project_name + " && make clean && make fast && make tests && make bin/run_all_tests && make programs -j7 && make examples -j7)";
+            for (auto &command : commands)
+            {
+               std::cout << "Running \"" << command << "\"" << std::endl;
+               std::string make_shell_command = command;
+               std::string make_shell_result = fetcher.execute_command(make_shell_command);
+            }
          }
+
+         show_message = true;
       }
 
       std::this_thread::sleep_for(std::chrono::seconds(polling_frequency_sec));
+
+      if (show_message) std::cout << celebrator.generate_synced_banner() << std::endl;
 
       project_num++;
       if (project_num >= project_names_to_check.size()) project_num = 0;
