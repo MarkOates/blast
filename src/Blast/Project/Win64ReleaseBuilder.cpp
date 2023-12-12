c42b6b7be1ba78ea99802941f51629359467aaa2
Mark Oates <markoates0@gmail.com>
2023-12-12 14:00:30 -0500

    * Have Win64ReleaseBuilder throw due to not being implemented

--- src/Blast/Project/Win64ReleaseBuilder.cpp
+++ src/Blast/Project/Win64ReleaseBuilder.cpp
@@ -2,6 +2,7 @@
 
 #include <Blast/Project/Win64ReleaseBuilder.hpp>
 
+#include <Blast/Errors.hpp>
 #include <Blast/ShellCommandExecutorWithCallback.hpp>
 
 
@@ -30,8 +31,13 @@ std::string Win64ReleaseBuilder::build_full_path_to_win64_release_builder_script
 
 void Win64ReleaseBuilder::run()
 {
-   std::string script_path = build_full_path_to_win64_release_builder_script();
-   Blast::ShellCommandExecutorWithCallback executor(script_path);
+   Blast::Errors::throw_error(
+      "Blast::Project::Win64ReleaseBuilder::run",
+      "This feature is not yet implemented. It would be great if you could move this here."
+   );
+
+   //std::string script_path = build_full_path_to_win64_release_builder_script();
+   //Blast::ShellCommandExecutorWithCallback executor(script_path);
 }
 
 
