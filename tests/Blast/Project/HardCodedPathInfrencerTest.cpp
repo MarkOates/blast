8d0ccd290509288d9e02f125bba8f77b285a5455
Mark Oates <markoates0@gmail.com>
2023-12-20 11:50:30 -0500

    * Add better testing on HardCodedPathInfrencer

--- tests/Blast/Project/HardCodedPathInfrencerTest.cpp
+++ tests/Blast/Project/HardCodedPathInfrencerTest.cpp
@@ -10,13 +10,16 @@ TEST(Blast_Project_HardCodedPathInfrencerTest, can_be_created_without_blowing_up
 }
 
 
-TEST(Blast_Project_HardCodedPathInfrencerTest, DISABLED__run__returns_the_expected_response)
+TEST(Blast_Project_HardCodedPathInfrencerTest,
+   check_for_hard_coded_paths__when_paths_are_detected__will_return_false_with_information_strings)
 {
    Blast::Project::HardCodedPathInfrencer hard_coded_path_infrencer;
-   hard_coded_path_infrencer.set_project_directory("/Users/markoates/Repos/Pipeline");
-   std::pair<bool, std::vector<std::string>> expected_result = { false, { "foo" } };
+   hard_coded_path_infrencer.set_project_directory("/Users/markoates/Repos/blast");
+   std::pair<bool, std::vector<std::string>> expected_result = { false, { "foo" } }; // TODO: Use actual info
    std::pair<bool, std::vector<std::string>> actual_result = hard_coded_path_infrencer.check_for_hard_coded_paths();
-   EXPECT_EQ(expected_result, actual_result);
+   EXPECT_EQ(expected_result.first, actual_result.first);
+   // TODO: Use this full assertion
+   //EXPECT_EQ(expected_result, actual_result);
 }
 
 
