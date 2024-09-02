dfedc481b0959f6fb15fd054715a2c3650678688
Mark Oates <markoates0@gmail.com>
2024-09-02 17:56:46 -0400

    Add "RotateWorld" to projects in status program

--- programs/status.cpp
+++ programs/status.cpp
@@ -400,6 +400,7 @@ int main(int argc, char **argv)
       //{ "fullscore",        { false, ProjectStatus("fullscore", base_dir) } },
       //{ "motris",               { false, ProjectStatus("motris", base_dir) } },
       //{ "tilemap",              { false, ProjectStatus("tilemap") } },
+      { "RotateWorld",  { false, ProjectStatus("RotateWorld", base_dir) } },
       { "Fade to White (Krampus21)", { false, ProjectStatus("Krampus21", base_dir) } },
       { ".dotfiles",        { false, ProjectStatus(".dotfiles", base_dir) } },
       { "Railway",  { false, ProjectStatus("Railway", base_dir) } },
